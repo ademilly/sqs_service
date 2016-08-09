cd6cb1e7ca904472376eafb8682621675c310f2e
Aurélien Demilly <aurelien.demilly@predicsis.com>
2016-08-09 12:02:23 +0200

    update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name='sqs_service',
-    version='0.2',
+    version='0.21',
     description='Python SQS Wrapper',
     author='Aurélien Demilly',
     author_email='demilly.aurelien@gmail.com',
